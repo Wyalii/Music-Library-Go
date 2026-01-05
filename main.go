d1c8294c09fb463c1581eab68e10c1d9a2eb32cc
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2026-01-05 13:18:17 -0400

    starting to mess around with bubbletea...

--- main.go
+++ main.go
@@ -2,107 +2,35 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 
-	imageToASCII "github.com/Wyalii/ASCII-Converter"
 	tea "github.com/charmbracelet/bubbletea"
-	// "github.com/charmbracelet/bubbles/spinner"
-	// "github.com/charmbracelet/lipgloss"
 )
 
-func main() {
-	imageToASCII.ConvertImageToASCII("/home/wyalii/Downloads/randomImage.jpg", 50)
-	p := tea.NewProgram(initialModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
-	}
-}
-
 type model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
-}
-
-func initialModel() model {
-	return model{
-		choices:  []string{"Music List", "Download Music", "Create Playlist"},
-		selected: make(map[int]struct{}),
-	}
+	message string
+	count   int
 }
 
 func (m model) Init() tea.Cmd {
 	return nil
 }
 func (m model) View() string {
-	// The header
-	s := "Welcome to GO MUSIC LIBRARY\n\n"
-
-	// Iterate over our choices
-	for i, choice := range m.choices {
-
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
-
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
-
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	}
-
-	// The footer
-	s += "\nPress q to quit.\n"
-
-	// Send the UI for rendering
-	return s
+	return fmt.Sprint(m.message, m.count)
 }
-
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
-	// Is it a key press?
 	case tea.KeyMsg:
-
-		// Cool, what was the actual key pressed?
 		switch msg.String() {
-
-		// These keys should exit the program.
-		case "ctrl+c", "q":
+		case "ctrl+c":
 			return m, tea.Quit
-
-		// The "up" and "k" keys move the cursor up
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
 		}
 	}
-
-	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
 	return m, nil
+}
+func main() {
+	p := tea.NewProgram(model{message: "this is the internal state: ", count: 0})
+	if _, teaErr := p.Run(); teaErr != nil {
+		log.Panic(teaErr)
+	}
 }
