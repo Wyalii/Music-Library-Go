9b24c9fa0ef6ca260605bbdd7e651d027f6c448e
Wyalii <gigaurigiorgi2727@gmail.com>
2026-02-13 15:48:10 -0400

    added pages and focus switching between page and main menu.

--- cmd/main.go
+++ cmd/main.go
@@ -12,22 +12,44 @@ func main() {
 	menu := tview.NewList().AddItem("Search", "Search Songs", 's', nil).AddItem("Library", "Your music library", 'l', nil).AddItem("Playlists", "Your playlists", 'p', nil)
 	menu.SetBorder(true).SetTitle("Main Menu")
 
-	pages := tview.NewPages()
-	searchPage := tview.NewTextView().SetText("Search Page Content")
-	searchPage.SetBorder(true).SetTitle("Search")
+	searchBar := tview.NewInputField().SetLabel("Search Songs:").SetFieldWidth(0)
 
-	libraryPage := tview.NewTextView().SetText("Library Page Content")
+	pages := tview.NewPages()
+	searchPage := tview.NewFlex()
+	searchPage.SetBorder(true).SetTitle("Search")
+	searchPage.AddItem(searchBar, 0, 1, true)
+
+	libraryPage := tview.NewFlex()
 	libraryPage.SetBorder(true).SetTitle("Library")
 
-	playlistsPage := tview.NewTextView().SetText("Playlist Page Content")
+	playlistsPage := tview.NewFlex()
 	playlistsPage.SetBorder(true).SetTitle("Playlists")
-
+	content := searchPage
 	pages.AddPage("Search", searchPage, true, true).AddPage("Library", libraryPage, true, true).AddPage("Playlists", playlistsPage, true, true)
 
 	menu.SetChangedFunc(func(index int, mainText string, secondary string, shortcut rune) {
 		pages.SwitchToPage(mainText)
+		switch mainText {
+		case "Search":
+			content = searchPage
+		case "Library":
+			content = libraryPage
+		case "Playlists":
+			content = playlistsPage
+		}
 	})
 
 	layout := tview.NewFlex().AddItem(menu, 25, 0, true).AddItem(pages, 0, 1, false)
-	app.SetRoot(layout, true).Run()
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyTab {
+			if app.GetFocus() == menu {
+				app.SetFocus(content)
+			} else {
+				app.SetFocus(menu)
+			}
+			return nil
+		}
+		return event
+	})
+	app.SetRoot(layout, true).SetFocus(menu).Run()
 }
